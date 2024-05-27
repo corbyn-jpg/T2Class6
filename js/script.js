f96ae2d109e113fc25fe86338115cf576e53f8a4
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2024-05-27 14:31:20 +0200

    good and bad arrays

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let numbers = [40, 100, 1];
+let points = new Array(40, 100, 1);
+
+console.log(numbers);
+console.log(points);
+
+//One item in an array
+let good = [40];
+
+//Creates spaces in the array
+let bad = new Array(40);
+
+console.log(good);
+console.log(bad);
